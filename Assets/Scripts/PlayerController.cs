857d9fc8de01209555fbcd24fdd2a2a967dabc85
Dominik Becker <dominik.becker5@googlemail.com>
2017-09-30 15:45:09 +0200

    Inital Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public Text collectedGemsText;
+    public Text winText;
+    public Text allGemsCollectedText;
+    public float timeToDisplayAllGemsCollectedText;
+    private int collectedGems = 0;
+    private int maxGems = 0;
+    private float startDisplayTime = 0;
+
+	// Use this for initialization
+	public void Start () {
+        rb = GetComponent<Rigidbody>();
+        maxGems = GameObject.FindGameObjectsWithTag("Gem").Length;
+        collectedGemsText.text = "Collected: " + collectedGems.ToString() + " of " + maxGems;
+    }
+
+    public void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = 0;
+        float moveZ = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveY = (float) 5;
+        }
+        Vector3 move = new Vector3(moveX, moveY, moveZ);
+
+        rb.AddForce(move * speed);
+
+        if (allGemsCollectedText.gameObject.activeSelf && (Time.time-startDisplayTime)>timeToDisplayAllGemsCollectedText)
+        {
+            allGemsCollectedText.gameObject.SetActive(false);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Gem":
+                collision.gameObject.SetActive(false);
+                collectedGems++;
+                collectedGemsText.text = "Collected: " + collectedGems.ToString() + " of "+maxGems;
+                if (collectedGems == maxGems)
+                {
+                    allGemsCollectedText.gameObject.SetActive(true);
+                    startDisplayTime = Time.time;
+                }
+                break;
+            case "Target":
+                if (collectedGems == maxGems)
+                {
+                    Application.Quit();
+                }
+                break;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Roll-a-Ball
+This is a simple rebuild of the official Unity Tutorial ["Roll a Ball"](https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial), aiming to demonstrate some basic features of the Unity Game Engine and the editor.
+
+It only uses Unity-internal meshes and does not rely on any 3rd party assets.
